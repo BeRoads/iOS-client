3929077044da2fef08e544c50ab521dd954d9085
Lionel Schinckus <valcapri@me.com>
2013-05-29 02:05:30 +0200

    - Ajout d'un bundle pour les settings et ses traductions - Ajout d'InAppSettingsKit pour que ce soit modifiable dans l'App aussi

---
+++ BeRoads/Classes/Controllers/LSSettingsViewController.h
@@ -0,0 +1,15 @@
+//
+//  LSSettingsViewController.h
+//  BeRoads
+//
+//  Created by Lionel Schinckus on 29/05/13.
+//  Copyright (c) 2013 Lionel Schinckus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IASKAppSettingsViewController.h"
+
+@interface LSSettingsViewController : IASKAppSettingsViewController <IASKSettingsDelegate>
+
+@end
---
+++ BeRoads/Classes/Controllers/LSSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  LSSettingsViewController.m
+//  BeRoads
+//
+//  Created by Lionel Schinckus on 29/05/13.
+//  Copyright (c) 2013 Lionel Schinckus. All rights reserved.
+//
+
+#import "LSSettingsViewController.h"
+
+@interface LSSettingsViewController ()
+
+@end
+
+@implementation LSSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
